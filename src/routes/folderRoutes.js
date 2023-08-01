6b2601dc92304d4726461939177a4e5d06a0cb03
Adolfo Angel Salomé Hernández <al222010588@gmail.com>
2023-08-01 13:55:37 -0600

    Lógica para logIn con jwt

--- src/routes/folderRoutes.js
+++ src/routes/folderRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { createFolder, addFileToFolder, deleteFolder, deleteFile, getFoldersAndFiles, getFile, getFilesInFolder } from '../controllers/folderController.js';
 import multer from 'multer';
+import * as middleAuth from '../middlewares/authJWT.js' 
 
 const router = express.Router();
 const upload = multer();
 
-router.post('/create/:parentFolder?', createFolder); //Ruta para crear subfolders
 // Endpoint to create a folder
-router.post('/create', createFolder); //Ruta para crear folder único
+router.post('/create', middleAuth.verifyToken ,createFolder); //Ruta para crear folder único
+router.post('/create/:parentFolder?', createFolder); //Ruta para crear subfolders
 
 // Endpoint to add a file to a folder
 router.post('/addFile/:folderName', upload.single('file'), addFileToFolder);
@@ -19,7 +20,7 @@ router.delete('/delete/:folderName', deleteFolder);
 router.delete('/delete/:folderName/:fileName', deleteFile);
 
 // Endpoint to get list of folders and files inside each folder
-router.get('/list', getFoldersAndFiles);
+router.get('/list',middleAuth.verifyToken, getFoldersAndFiles);
 
 router.get('/list/:ruta', getFile) //Busca y descarga el file de la ruta que desees
 
